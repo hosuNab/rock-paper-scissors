810a488a8daa794ad950d5e85bb22e67a79e16a7
hosuNab <hosunab@gmail.com>
2025-02-23 22:09:39 +0200

    Add function to randomize CPU choice
    CPU randomly chooses between rock, paper and scissors.

--- game.js
+++ game.js
@@ -1,1 +1,13 @@
-console.log("Hello Worldo")
+// randomizer for the computer to play the game against us
+function getComputerChoice() {
+    cpu = Math.floor(Math.random() * 3);
+    if (cpu === 1) {
+        return "The Computer has chosen: Rock!";
+    } else if (cpu === 2) {
+        return "The Computer has chosen: Paper!";
+    } else {
+        return "The Computer has chosen: Scissors!";
+    }
+}
+
+console.log(getComputerChoice())
