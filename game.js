e882b7b73b3dc75a783d186cd11ac63bdd25c4af
hosuNab <hosunab@gmail.com>
2025-03-02 14:50:31 +0200

    Add game parameters and announcements
    Added five round logic for the game. Human can win, computer can win or
    there can be a draw. There will be starting announcement, announcements
    between the rounds and announcement for the end of the game. Game done!

--- game.js
+++ game.js
@@ -1,56 +1,78 @@
+// Global score values, starts at 0
 var humanScore = 0;
 var computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-    // player's choice
-    function getHumanChoice() {
-        return humanChoice = prompt("Rock, Paper, Scissors?").toLowerCase();
-    }
+// Greetings at the start of the game
+console.log("A game of rock-paper-scissors shall be played! Five rounds! May the odds ever be in your favor!")
 
-    // randomizer for the computer to play the game against us
-    function getComputerChoice() {
-        cpu = Math.floor(Math.random() * 3);
-        if (cpu === 0) {
-            return computerChoice = "rock";
-        } else if (cpu === 1) {
-            return computerChoice = "paper";
+function playGame() {
+    function playRound(humanChoice, computerChoice) {
+        // Player's choice: prompt
+        function getHumanChoice() {
+            return humanChoice = prompt("Please choose: Rock, Paper or Scissors").toLowerCase();
+        }
+
+        // Computer's choice: randomizer for the computer to play the game against us
+        function getComputerChoice() {
+            cpu = Math.floor(Math.random() * 3);
+            if (cpu === 0) {
+                return computerChoice = "rock";
+            } else if (cpu === 1) {
+                return computerChoice = "paper";
+            } else {
+                return computerChoice = "scissors";
+            }
+        }
+
+        getHumanChoice();
+        getComputerChoice();
+
+        // Round announcement, tells what each player chose
+        console.log("Player chose " + humanChoice + "!");
+        console.log("Computer chose " + computerChoice + "!");
+
+        // Game logic, rock > scissors, scissors > paper, paper > rock
+        if (humanChoice === computerChoice) {
+            return "Draw!";
+        } else if (humanChoice === "rock" && computerChoice === "scissors") {
+            humanScore++;
+            return "1 point for human! Rock beats scissors!";
+        } else if (humanChoice === "rock" && computerChoice === "paper") {
+            computerScore++;
+            return "1 point for computer! Paper beats rock!";
+        } else if (humanChoice === "paper" && computerChoice === "rock") {
+            humanScore++;
+            return "1 point for human! Paper beats rock!";
+        } else if (humanChoice === "paper" && computerChoice === "scissors") {
+            computerScore++;
+            return "1 point for computer! Scissors beats paper!";
+        } else if (humanChoice === "scissors" && computerChoice === "paper") {
+            humanScore++;
+            return "1 point for human! Scissors beats paper!";
+        } else if (humanChoice === "scissors" && computerChoice === "rock") {
+            computerScore++;
+            return "1 point for computer! Rock beats scissors!";
         } else {
-            return computerChoice = "scissors";
+            return "Please choose rock, paper or scissors. There are rules you know!";
         }
     }
 
-    getHumanChoice();
-    getComputerChoice();
-
-    console.log("Player chose " + humanChoice + "!");
-    console.log("Computer chose " + computerChoice + "!");
-
-    if (humanChoice === computerChoice) {
-        return "Draw!";
-    } else if (humanChoice === "rock" && computerChoice === "scissors") {
-        humanScore++;
-        return "1 point for human! Rock beats scissors!";
-    } else if (humanChoice === "rock" && computerChoice === "paper") {
-        computerScore++;
-        return "1 point for computer! Paper beats rock!";
-    } else if (humanChoice === "paper" && computerChoice === "rock") {
-        humanScore++;
-        return "1 point for human! Paper beats rock!";
-    } else if (humanChoice === "paper" && computerChoice === "scissors") {
-        computerScore++;
-        return "1 point for computer! Scissors beats paper!";
-    } else if (humanChoice === "scissors" && computerChoice === "paper") {
-        humanScore++;
-        return "1 point for human! Scissors beats paper!";
-    } else if (humanChoice === "scissors" && computerChoice === "rock") {
-        computerScore++;
-        return "1 point for computer! Rock beats scissors!";
-    } else {
-        return "Error, I don't know how this happened!"
-    }
+    playRound();
 }
 
-console.log(playRound())
-console.log(humanScore)
-console.log(computerScore)
+// Game loop, play five rounds
+for (let round = 0; round < 5; round++) {
+    console.log("The score is: Human " + humanScore + " - " + computerScore + " Computer")
+    playGame();
+}
 
+// Final score and the announcement of the winner, ends the game
+console.log("The game has ended! The final tally: Human " + humanScore + " - " + computerScore + " Computer");
+
+if (humanScore === computerScore) {
+    console.log("It's a draw!");
+} else if (humanScore > computerScore) {
+    console.log("Human wins!");
+} else {
+    console.log("Computer wins!");
+}
